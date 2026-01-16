23c60fa01434d480ea770432ae4e3167a6495a71
Yaniry Florez <ymflorez@gmail.com>
2026-01-16 14:40:08 -0500

    Taller25-YFlorez_ Crud de Api Matricula

---
+++ src/main/java/uce/edu/web/api/matricula/application/EstudianteService.java
@@ -0,0 +1,19 @@
+package uce.edu.web.api.matricula.application;
+
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import uce.edu.web.api.matricula.domain.Estudiante;
+import uce.edu.web.api.matricula.infraestructure.EstudianteRepository;
+
+@ApplicationScoped
+public class EstudianteService {
+    @Inject
+    EstudianteRepository estudianteRepository;
+
+    public List<Estudiante> listarTodos() {
+        return estudianteRepository.listAll();
+    }
+
+}
---
+++ src/main/java/uce/edu/web/api/matricula/domain/Estudiante.java
@@ -0,0 +1,24 @@
+package uce.edu.web.api.matricula.domain;
+
+import java.time.LocalDate;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "estudiantes")
+@SequenceGenerator(name = "seq_estudiante", sequenceName = "seq_estudiante", allocationSize = 1)
+public class Estudiante extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_estudiante")
+    public Integer id;
+    public String nombre;
+    public String apellido;
+    public LocalDate fechaNacimiento;
+
+}
